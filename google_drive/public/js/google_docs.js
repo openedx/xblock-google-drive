7e5218da639be900f13ca6b3de11d2e3c14f27f9
marjev <marjevtic@gmail.com>
2014-12-02 11:41:57 +0100

    Reporting event for analytics

--- google_drive/public/js/google_docs.js
+++ google_drive/public/js/google_docs.js
@@ -1,6 +1,24 @@
 /* Javascript for GoogleDocumentBlock. */
 function GoogleDocumentBlock(runtime, element) {
 
+    $('iframe', element).load(function(){
+        var iframe_url = $(this).attr('src');
+        $.ajax({
+            type: "POST",
+            url: runtime.handlerUrl(element, 'iframe_loaded'),
+            data: JSON.stringify({url: iframe_url})
+        });
+    });
+
+    $('img', element).load(function(){
+        var image_url = $(this).attr('src');
+        $.ajax({
+            type: "POST",
+            url: runtime.handlerUrl(element, 'image_loaded'),
+            data: JSON.stringify({url: image_url})
+        });
+    });
+
     $(function ($) {
         /* Here's where you'd do things on page load. */
     });

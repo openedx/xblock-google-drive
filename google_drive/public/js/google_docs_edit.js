58631fa1f46c36a01d521444afd74288b7570feb
Martyn James <mjames@edx.org>
2014-12-05 15:35:48 -0500

    Fixes problem when loading document edit box

--- google_drive/public/js/google_docs_edit.js
+++ google_drive/public/js/google_docs_edit.js
@@ -1,4 +1,4 @@
-function GoogleDocumentEditBlock(runtime, element, defaults) {
+function GoogleDocumentEditBlock(runtime, element) {
 
     var clear_name_button = $('.clear-display-name', element);
     var save_button = $('.save-button', element);

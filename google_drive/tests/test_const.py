947815d3ddf23905466436a337aca24c3babff94
Waheed Ahmed <waheed.ahmed@arbisoft.com>
2019-10-21 16:05:31 +0500

    Update test image link from spartans-team drive.
    PROD-773

--- google_drive/tests/test_const.py
+++ google_drive/tests/test_const.py
@@ -15,5 +15,5 @@ STATUS_CODE_200 = {'status_code': 200}
 STATUS_CODE_400 = {'status_code': 400}
 STATUS_CODE_404 = {'status_code': 404}
 
-TEST_IMAGE_URL = 'https://docs.google.com/drawings/d/e/2PACX-1vR5IEdW9QtEDQG8Kr0ZyigIVJNE8PTpet3H1AcLH0LsCBIVlN' \
-                 '-onsTnmP5cfD44cwzrAS3jacvPP6q6/pub?w=960&h=720'
+TEST_IMAGE_URL = 'https://docs.google.com/drawings/d/e/2PACX-1vTDskPsAcSoDz6D0swCEuf9n7R67X0zuaDLIrDorbon9JhmIjtW' \
+                 '_1bm0PbjcU7M2xzUOXA3asuSih2t/pub?w=960&h=720'
